cffb5f675e5e59156f9f6614071911adeba16412
Julian <julian.bottiglione@epitech.eu>
2022-11-08 20:31:57 +0100

    julian.h is born

---
+++ includes/julian.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** Julian H
+** File description:
+** the ultimate .h file you have ever seen
+*/
+
+#include "my.h"
+#include "my_printf.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/sysmacros.h>
