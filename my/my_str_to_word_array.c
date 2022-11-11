8dc20a77ff7e576795e9d157ec8a8ade5ed4c89e
Julian <julian.bottiglione@epitech.eu>
2022-11-11 19:13:01 +0100

    [add] no seg fault, still needs some testing

--- my/my_str_to_word_array.c
+++ my/my_str_to_word_array.c
@@ -5,21 +5,24 @@
 ** Flat str becomes huge array
 */
 
+#include <stdlib.h>
+int my_strlen(char const *str);
+char *my_strcpy(char *dest, char const *src);
 
 char **my_str_to_word_array(char const *str)
 {
     int a = 0;
     int s = 0;
-    char **array = malloc(strlen(str) * 10);
-    char *string = malloc(strlen(str));
+    char **array = malloc(my_strlen(str) * 10);
+    char *string = malloc(my_strlen(str));
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] == '\n'|| str[i] == ' ') {
             string[s++] = '\0';
-            array[a] = malloc(strlen(string) + 1);
+            array[a] = malloc(my_strlen(string) + 1);
             my_strcpy(array[a++], string);
-      
+
             free(string);
-            string = malloc(strlen(str+i));
+            string = malloc(my_strlen(str+i));
             s = 0;
             continue;
         }
