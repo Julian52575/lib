e13689cc835220f2b6fbc28d478289ad65bacfb2
Julian <julian.bottiglione@epitech.eu>
2022-11-12 15:10:30 +0100

    [add] put array works

--- my/my_put_array.c
+++ my/my_put_array.c
@@ -8,10 +8,10 @@ void my_putchar(char c);
 
 void my_put_array(char **array, char del)
 {
-    for (int y = 0; array[y][0] != '\0'; y++) {
-        for (int x = 0; array[y][x] != '\0'; x++) {
-            my_putchar(array[y][x]);
-        }
-        my_putchar(del);
+  for (int y = 0; array[y][0] != '\0'; y++) {
+    for (int x = 0; array[y][x] != '\0'; x++) {
+      my_putchar(array[y][x]);
     }
+    my_putchar(del);
+  }
 }
