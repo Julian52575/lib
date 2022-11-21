22ae9933e3f83ad11bd1f8ec1023dc0ad44f3ef2
Julian <julian.bottiglione@epitech.eu>
2022-11-21 20:09:59 +0100

    [fix] no more coding style error

--- my/my_str_to_word_array.c
+++ my/my_str_to_word_array.c
@@ -17,19 +17,17 @@ char **my_str_to_word_array(char const *str)
     char **array = malloc(my_strlen(str) * 10);
     char *string = malloc(my_strlen(str));
     for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == '\n'|| str[i] == ' ') {
+        if (str[i] == '\n' || str[i] == ' ') {
             string[s++] = '\0';
             array[a] = malloc(my_strlen(string) + 1);
             my_strcpy(array[a++], string);
 
             free(string);
-            string = malloc(my_strlen(str+i));
+            string = malloc(my_strlen(str + i));
             s = 0;
             continue;
-        }
-        string[s++] = str[i];
-    }
-    string[s] = '\0';
+        } string[s++] = str[i];
+    } string[s] = '\0';
     array[a++] = string;
     array[a] = "\0";
     return array;
