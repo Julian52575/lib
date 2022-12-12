0a60891659ef12d467f22d10a3120e7bd07b7b8e
Julian <julian.bottiglione@epitech.eu>
2022-12-12 15:51:35 +0100

    [fix] printf in its own folder

--- my/my_get_digits.c
+++ my/my_get_digits.c
@@ -7,18 +7,15 @@
 
 int get_1st_digit(int nb)
 {
-    printf("%d\n", nb / 100);
     return (nb / 100);
 }
 
 int get_2nd_digit(int nb)
 {
-    printf("%d\n", (nb / 10) % 10);
     return( (nb / 10) % 10);
 }
 
 int get_3rd_digit(int nb)
 {
-    printf("%d\n", nb % 10);
     return (nb % 10);
 }
