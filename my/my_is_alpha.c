56a6b5fdaa30021ad578271a0c395656fbae23e7
Julian <julian.bottiglione@epitech.eu>
2022-12-25 15:59:05 +0100

    [add] changed build.sh rights and updated my_is_alpa

--- my/my_is_alpha.c
+++ my/my_is_alpha.c
@@ -5,16 +5,22 @@
 ** are the 2str in alphabetical order ?
 */
 
+//return 1 if the 2 str are in alphabetical order, 0 if not.
 int my_is_alpha(char *str1, char *str2)
 {
-    int i = 0;
-    while (1) {
-        if (str1[i] == '\0')
-            return 1;
-        if (str2[i] == '\0')
-            return 0;
-        if (str1[i] > str2[i])
-            return 0;
-        i++;
+    if (str1[0] == '\0') {
+        return 1;
+    } else if (str2[0] == '\0') {
+        return 0;
     }
+
+    if (65 <= str1[0] && str1[0] <= 90) {
+        str1[0] += 32;
+    } else if (65 <= str2[0] && str2[0] <= 90) {
+        str2[0] += 32;
+    }
+
+    if (str1[0] > str2[0]) return 0;
+    if (str1[0] == str2[0]) return my_is_alpha(str1 + 1, str2 + 1);
+    return 1;
 }
