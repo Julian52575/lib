ad3c7438f986bc1e5ad3fd55465d89f6065b7c6a
Julian <julian.bottiglione@epitech.eu>
2023-01-02 15:07:42 +0100

    [add] new functions

--- my/my_str_compare.c
+++ my/my_str_compare.c
@@ -1,16 +1,11 @@
 /*
-11;rgb:3030/0a0a/2424** EPITECH PROJECT, 2022
+** EPITECH PROJECT, 2022
 ** my str compare
 ** File description:
 ** 1 if both are the same
 */
+static int my_str_compare2(char s1, char s2);
 
-static int my_str_compare2(char s1, char s2)
-{
-  if (s1 != s2) return 0;
-  return 1;
-}
-  
 //returns 1 if both str are the same, 0 if not
 int my_str_compare(char *str1, char *str2)
 {
@@ -23,4 +18,10 @@ int my_str_compare(char *str1, char *str2)
     if (str1[0] && !str2[0]) return 0;
     if (!str1[0] && str2[0]) return 0;
     return a;
+}
+
+static int my_str_compare2(char s1, char s2)
+{
+    if (s1 != s2) return 0;
+    return 1;
 }
